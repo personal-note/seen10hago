64ffbaafb893d770118ef9f3ae63b362fcec5bde
personalnote <93585461+personal-note@users.noreply.github.com>
2024-06-27 10:34:22 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,44 @@
 namespace SpriteKind {
     export const UI = SpriteKind.create()
 }
+function music2 () {
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(494, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Whole))
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(494, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Whole))
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(494, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(370, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(370, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Whole))
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(494, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Whole))
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(494, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(494, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Whole))
+    music.play(music.tonePlayable(370, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(330, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
+controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
+    Render.toggleViewMode()
+})
+music.setTempo(80)
 let gate1 = true
 let gate2 = true
 let gate3 = true
@@ -57,23 +95,26 @@ game.showLongText("*something crashes in the background*", DialogLayout.Bottom)
 game.onUpdate(function () {
     if (mySprite.tilemapLocation() == tiles.getTileLocation(4, 6)) {
         if (gate1) {
-        	
+            game.showLongText("Why did you leave me behind?", DialogLayout.Bottom)
         }
     } else if (mySprite.tilemapLocation() == tiles.getTileLocation(4, 9)) {
         if (gate2) {
-        	
+            game.showLongText("Why am I always second?", DialogLayout.Bottom)
         }
     } else if (mySprite.tilemapLocation() == tiles.getTileLocation(6, 10)) {
         if (gate3) {
-        	
+            game.showLongText("Did you ever think about me?", DialogLayout.Bottom)
         }
     } else if (mySprite.tilemapLocation() == tiles.getTileLocation(9, 10)) {
         if (gate1) {
-        	
+            game.showLongText("I know you saw me", DialogLayout.Bottom)
         }
     } else if (mySprite.tilemapLocation() == tiles.getTileLocation(0, 0)) {
         if (gate1) {
         	
         }
     }
+})
+forever(function () {
+    music2()
 })
