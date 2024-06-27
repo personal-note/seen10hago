2c9c438a025fb296121cb7b6304890677afaa25c
personalnote <93585461+personal-note@users.noreply.github.com>
2024-06-27 09:26:16 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 namespace SpriteKind {
     export const UI = SpriteKind.create()
 }
+let gate1 = true
+let gate2 = true
+let gate3 = true
+let gate4 = true
+let gate5 = true
 let mySprite = Render.getRenderSpriteVariable()
 tiles.setCurrentTilemap(tilemap`level2`)
 Render.changeBrightness(3, 1.4)
@@ -49,3 +54,26 @@ game.setDialogCursor(img`
     . . . . . . . . . . . . . . . . 
     `)
 game.showLongText("*something crashes in the background*", DialogLayout.Bottom)
+game.onUpdate(function () {
+    if (mySprite.tilemapLocation() == tiles.getTileLocation(4, 6)) {
+        if (gate1) {
+        	
+        }
+    } else if (mySprite.tilemapLocation() == tiles.getTileLocation(4, 9)) {
+        if (gate2) {
+        	
+        }
+    } else if (mySprite.tilemapLocation() == tiles.getTileLocation(6, 10)) {
+        if (gate3) {
+        	
+        }
+    } else if (mySprite.tilemapLocation() == tiles.getTileLocation(9, 10)) {
+        if (gate1) {
+        	
+        }
+    } else if (mySprite.tilemapLocation() == tiles.getTileLocation(0, 0)) {
+        if (gate1) {
+        	
+        }
+    }
+})
